1a264d696b016c19391a226383173f4d338a1262
Adam Tate <lincolnluxor@gmail.com>
2014-02-01 14:28:24 -0800

    color changes and added in a logo

--- app/src/main/java/com/galavantier/app/FullscreenActivity.java
+++ app/src/main/java/com/galavantier/app/FullscreenActivity.java
@@ -111,7 +111,7 @@ public class FullscreenActivity extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.create_account_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
